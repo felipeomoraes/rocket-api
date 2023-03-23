13063930b655629d4613d934e1f57379eaf13c66
Felipe Moraes <felipemoraesdev@gmail.com>
2023-03-22 21:15:12 -0300

    Configuração do Knex e variáveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,41 @@
 import fastify from "fastify";
-import { knex } from "./db";
+import crypto from 'node:crypto';
+import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
 app.get("/", async (req, res) => {
 
-    const tables = await knex('sqlite_schema').select('*');
+    const transactions = await knex('transactions').insert({
+        id: crypto.randomUUID(),
+        title: 'Transaction test',
+        amount: 1000
+    }).returning('*');
 
-    res.send(tables);
+    res.send(transactions);
+});
+
+app.get("/consult", async (req, res) => {
+
+    const transactions = await knex('transactions').select('*');
+
+    res.send(transactions);
+});
+
+app.get("/consult/:amount", async (req, res) => {
+
+    const { amount } = req.params;
+
+    const transactions = await knex('transactions')
+        .where('amount', amount)
+        .select('*');
+
+    res.send(transactions);
 });
 
 app.listen({
-    port: 3333
+    port: env.PORT
 }).then(() => {
-    console.log("Server listening on port 3333");
+    console.log(`Server listening on port ${env.PORT}`);
 });
